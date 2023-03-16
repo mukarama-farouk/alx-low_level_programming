d5647cce4cd45d53c0d6c239e99e48259e80e4a4
mukarama-farouk <faroukmukarama@gmail.com>
2023-03-16 11:30:03 -0700

    4.Hello , puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,8 @@
 #!/bin/bash
-#include<stdio.h>
-/**
- * main - Entry point
- *
- * Return: Alaways 0 (Success)
- */
+#include <stdio.h>
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
 
-int main(void) {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+	return (0);
 }
