7b3307b8be80edcd42cb44fb1f1585f6ab54e5da
mukarama-farouk <faroukmukarama@gmail.com>
2023-04-07 08:33:00 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Name of program
+ * @argc: number of arguments
+ * @argv: Array of arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+(void) argv; /*Ignore argv*/													printf("%d\n", argc - 1);
+
+
+return (0);
+}
