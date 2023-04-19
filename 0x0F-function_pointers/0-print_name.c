e37c17d92797aa0413ef5cafaff9dee7e7153ee1
mukarama-farouk <faroukmukarama@gmail.com>
2023-04-19 09:23:34 -0700

    file 0 and README.md file added

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - function that prints a name
+ * @name: input
+ * @f: function pointer
+ *
+ * Return: Nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (f == NULL && name == NULL)
+		return;
+	f(name);
+}
