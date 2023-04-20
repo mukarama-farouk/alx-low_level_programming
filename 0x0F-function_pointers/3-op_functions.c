d6880e196c74016071b0d5b41487022afc567eed
mukarama-farouk <faroukmukarama@gmail.com>
2023-04-19 22:30:45 -0700

    update

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,5 +1,4 @@
 #include "3-calc.h"
-#include <stdio.h>
 
 int op_add(int a, int b);
 int op_sub(int a, int b);
@@ -48,14 +47,9 @@ int op_mul(int a, int b)
  * Return: The quotient of a and b.
  */
 int op_div(int a, int b)
-{
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
 	return (a / b);
-}
+
+
 /**
  * op_mod - Returns the remainder of the division of two numbers.
  * @a: The first number.
@@ -64,11 +58,4 @@ int op_div(int a, int b)
  * Return: The remainder of the division of a by b.
  */
 int op_mod(int a, int b)
-{
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
 	return (a % b);
-}
