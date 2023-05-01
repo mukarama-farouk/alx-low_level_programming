81084c36a7fc99e5dcd934c8ebfe48ef22000fdf
mukarama-farouk <faroukmukarama@gmail.com>
2023-05-01 02:18:30 -0700

    1-listint_len.c added

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stddef.h>
+
+/**
+ * listint_len - Function that returns the number of elements in a list
+ * @h: Pointer of list
+ *
+ * Return: Number of elements
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t all_nodes = 0;
+
+	while (h != NULL)
+	{
+		all_nodes++;
+		h = h->next;
+	}
+	return (all_nodes);
+}
