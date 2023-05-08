880e456571beb6693cd37a1d55673fe98f24468f
mukarama-farouk <faroukmukarama@gmail.com>
2023-05-08 11:02:54 -0700

    2-append_text_to_file.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,5 +1,7 @@
 #include "lists.h"
 
+
+
 /**
  * print_listint_safe - prints a link list
  * @head: head of list
@@ -8,17 +10,4 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-       	const listint_t *current = head;
-	size_t node = 0;
-
-	while (current != NULL)
-	{
-		printf("[%p]%d\n", (void *)current, current->n);
-		node++;
-
-		if (current <= current->next)
-			exit (98);
-		current = current->next;
-	}
-	return (node);
-}
+	listint_t *current = head;
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -20,7 +20,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (fptr == -1)
 		return (-1);
 
-	if (text_content == NULL)
+	if (text_content)
 	{
 		for (ch = 0; text_content[ch]; ch++)
 			;
