74080c0d3fb0f032440391a90e2d39307276bef8
mukarama-farouk <faroukmukarama@gmail.com>
2023-07-06 14:23:55 -0700

    update

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -17,7 +17,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (new_table == NULL)
 		return (NULL);
 
-	array = malloc(sizeof(hash_table_t *) * size);
+	array = malloc(sizeof(hash_node_t *) * size);
 	if (array == NULL)
 		return (NULL);
 
