e52ff705d5d0588417e3f0b616c1eeae6700547e
mukarama-farouk <faroukmukarama@gmail.com>
2023-07-06 13:03:33 -0700

    update

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,13 +9,21 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table;
+	hash_table_t *entries;
+	unsigned int i;
 
 	new_table = malloc(sizeof(hash_table_t));
 
 	if (new_table == NULL)
-		return NULL;
+		return (NULL);
 
+	entries = malloc(sizeof(hash_table_t *) * size);
+	if (entries == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		entries[i] = NULL;
+	new_table->entries = entries;
 	new_table->size = size;
 
-	return new_table;
+	return (new_table);
 }
