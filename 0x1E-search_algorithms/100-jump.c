01647d6858c17af095739787a9244e524396f051
mukarama-farouk <faroukmukarama@gmail.com>
2023-10-11 07:01:20 +0000

    100-jump.c

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -28,8 +28,8 @@ int jump_search(int *array, size_t size, int value)
 	for (i = left; i < (int)size && i <= right; i++)
 	{
 		printf("Value checked array[%d] = [%d]\n", i, array[i]);
-    		if (array[i] == value)
-        		return (i);
+		if (array[i] == value)
+			return (i);
 	}
 	return (-1);
 }
